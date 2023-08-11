01318721bef1bf602d5c9dfe11fa5251d4a557e5
Richard Molloy <richardmolloy@gmail.com>
2023-08-12 08:59:38 +1200

    Merge eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,29 @@
 module.exports = {
+    "root": true,
     env: {
+        "node": true,
+        "jest": true,
         es2022: true,
     },
+    "extends": [
+        "plugin:vue/base",
+        "plugin:vue/essential",
+        "eslint:recommended"
+    ],
     rules: {
         'no-console': 'off',
         'vue/no-side-effects-in-computed-properties': 'warn',
         'radix': 'error',
     },
-    "extends": ["plugin:vue/base"],
-    "plugins": ["jest"],
-};
+    "overrides": [
+        {
+            "files": [
+                "**/__tests__/*.{j,t}s?(x)",
+                "**/tests/unit/**/*.spec.{j,t}s?(x)"
+            ],
+            "env": {
+                "jest": true
+            }
+        }
+    ]
+}
