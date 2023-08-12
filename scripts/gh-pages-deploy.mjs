bbc27b38774520825bb8827155585b01e906cb1e
Richard Molloy <richardmolloy@gmail.com>
2023-08-12 15:10:38 +1200

    Update deploy scripts

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -1,21 +1,21 @@
-import {execa} from "execa";
+import { execa } from 'execa';
 
 (async () => {
-    try {
-        await execa("git", ["checkout", "--orphan", "gh-pages"]);
-        console.log("Building...");
-        await execa("npm", ["run", "build"]);
-        const folderName = "dist";
-        await execa("git", ["--work-tree", folderName, "add", "--all"]);
-        await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
-        console.log("Pushing to gh-pages...");
-        await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-        await execa("rm", ["-r", folderName]);
-        await execa("git", ["checkout", "-f", "master"]);
-        await execa("git", ["branch", "-D", "gh-pages"]);
-        console.log("Successfully deployed");
-    } catch (e) {
-        console.log(e.message);
-        process.exit(1);
-    }
-})();
+  try {
+    await execa('git', ['checkout', '--orphan', 'gh-pages'])
+    console.log('Building...')
+    await execa('npm', ['run', 'build'])
+    const folderName = 'dist'
+    await execa('git', ['--work-tree', folderName, 'add', '--all'])
+    await execa('git', ['--work-tree', folderName, 'commit', '-m', 'gh-pages'])
+    console.log('Pushing to gh-pages...')
+    await execa('git', ['push', 'origin', 'HEAD:gh-pages', '--force'])
+    await execa('rm', ['-r', folderName])
+    await execa('git', ['checkout', '-f', 'master'])
+    await execa('git', ['branch', '-D', 'gh-pages'])
+    console.log('Successfully deployed')
+  } catch (e) {
+    console.log(e.message)
+    process.exit(1)
+  }
+})()
