133eb6ab31f7e972c4da5550edec382b190617e5
Richard Molloy <richardmolloy@gmail.com>
2023-08-12 17:41:45 +1200

    Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    "root": true,
-    env: {
-        "node": true,
-        es2022: true,
-    },
-    "extends": [
-        "plugin:vue/recommended",
-        "eslint:recommended",
-        "standard"
-    ],
-    rules: {
-        'no-console': 'off',
-        'vue/no-side-effects-in-computed-properties': 'warn',
-        'radix': 'error',
-    },
-    "overrides": [
-        {
-            "files": [
-                "**/__tests__/*.{j,t}s?(x)",
-                "**/tests/unit/**/*.spec.{j,t}s?(x)"
-            ]
-        }
-    ]
+  root: true,
+  env: {
+    node: true,
+    es2022: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'standard'
+  ],
+  rules: {
+    'no-console': 'off',
+    'vue/no-side-effects-in-computed-properties': 'warn',
+    radix: 'error'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ]
+    }
+  ]
 }
