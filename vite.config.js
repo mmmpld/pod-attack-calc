ffd4c396e5fb6b295f6cd2ae872a573cbbdc8c93
Richard Molloy <richardmolloy@gmail.com>
2023-08-12 15:23:11 +1200

    Set vite base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 
 const path = require('path')
 export default defineConfig({
+  base: '/pod-attack-calc/',
   plugins: [
     createVuePlugin(),
     Components({
